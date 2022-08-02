4c7081ea7a7f4a7f78834c5a700fcb36cfe9abe6
crisstanza <crisstanza@users.noreply.github.com>
2022-08-02 13:29:06 -0300

    Circle of Fifths - C Major.

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ https://crisstanza.github.io/my.piano.charts/
 ## Instagram
 
 https://www.instagram.com/my.piano.charts/
+
+
+### See Also
+
+https://web.library.yale.edu/cataloging/music/names-keys-french-german-italian-and-spanish
--- js/i18n.js
+++ js/i18n.js
@@ -3,8 +3,6 @@
 	const keys = {
 		languages: [ 'us', 'br', 'fr', 'es' ],
 		values: {
-			'Backing-Tracks': ['Backing Tracks', 'Pistas de Acompanhamento', 'Pistes d\'Accompagnement', 'Pistas de Acompa&ntilde;amiento' ],
-			'...back': [ '...back', '...voltar', '...retour', '...de vuelta' ],
 			'left-hand': [ 'left hand', 'm&atildeo esquerda', 'main gauche', 'mano izquierda' ],
 			'right-hand': [ 'right hand', 'm&atildeo direita', 'main droite', 'mano derecha' ],
 			'thumb': [ 'thumb', 'polegar', 'le pouce', 'pulgar' ],
@@ -12,10 +10,18 @@
 			'middle': [ 'middle', 'm&eacute;dio', 'le majeur', 'medio' ],
 			'ring': [ 'ring', 'anelar', 'l\'annulaire', 'anular' ],
 			'little': [ 'little', 'm&iacute;nimo', 'l\'auriculaire', 'me&ntilde;ique' ],
-			'parabens-pra-voce': [ 'Happy Birthday to You', 'Parab&eacute;ns pra Voc&ecirc;', 'Happy Birthday to You', 'Happy Birthday to You' ],
-			'title.parabens-pra-voce': [ 'Happy Birthday to You - My Piano Charts', 'Parab&eacute;ns pra Voc&ecirc; - My Piano Charts', 'Happy Birthday to You - My Piano Charts', 'Happy Birthday to You - My Piano Charts' ]
-		}
 
+			'Backing-Tracks': ['Backing Tracks', 'Pistas de Acompanhamento', 'Pistes d\'Accompagnement', 'Pistas de Acompa&ntilde;amiento' ],
+
+			'parabens-pra-voce': [ 'Happy Birthday to You', 'Parab&eacute;ns pra Voc&ecirc;', 'Happy Birthday to You', 'Happy Birthday to You' ],
+			'title.parabens-pra-voce': [ 'Happy Birthday to You - My Piano Charts', 'Parab&eacute;ns pra Voc&ecirc; - My Piano Charts', 'Happy Birthday to You - My Piano Charts', 'Happy Birthday to You - My Piano Charts' ],
+
+			'Circle-of-Fifths': [ 'Circle of Fifths', 'Ciclo das Quintas', 'Cycle des Quintes', 'Ciclo de Quintas' ],
+			'c-major': [ 'C Major', 'D&oacute; Maior', 'Do Majeur', 'Do Mayor' ],
+			'title.c-major_circle-of-fifths': [ 'C Major - Circle of Fifths - My Piano Charts', 'D&oacute; Maior - Ciclo das Quintas - My Piano Charts', 'Do Majeur - Cycle des Quintes - My Piano Charts', 'Do Mayor - Do Mayor - My Piano Charts' ],
+
+			'...back': [ '...back', '...voltar', '...retour', '...de vuelta' ],
+		}
 	};
 
 	function init(event) {
